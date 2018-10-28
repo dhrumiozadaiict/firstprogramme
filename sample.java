575f609eb8d20ac9184b20f831cea79513305411
Dhrumil Oza <dhrumil.oza.22@gmail.com>
2018-10-28 14:19:33 +0530

    Alternate change

--- sample.java
+++ sample.java
@@ -2,7 +2,8 @@ import java.io.*;
 import java.util.*;
 class sample{
     public static void main(String[] args) {
-        int x = 5;
-        System.out.println(x);
+        int x = 7;
+        int y = 9;
+        System.out.println(x+" "+y);
     }
 }
